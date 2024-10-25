b3684a30c95cf06624f5fa84af502eda364764d5
Israel Curiel <israelcuriel597@hotmail.com>
2024-10-25 13:24:12 -0600

    LGG: Reglas de negocio (Transactions) Agregada

---
+++ LGG.LenayGestorGatos.Domain/Interfaces/Services/ITransactionPresenter.cs
@@ -0,0 +1,43 @@
+﻿/// Developer : Israel Curiel
+/// Creation Date : 25/10/2024
+/// Creation Description:Interface
+/// Update Date : --/--/----
+/// Update Description : ------
+///CopyRight: Lenay gestor de gastos
+namespace LGG.LenayGestorGatos.Domain.Interfaces.Services
+{
+    public interface ITransactionPresenter
+    {
+        /// <summary>
+        /// Agrega un registro a la tabla Gasto
+        /// </summary>
+        /// <returns></returns>
+        Task<RespuestaDB> AddGasto(TransactionAggregate aggregate, string token);
+
+        /// <summary>
+        /// Agrega un registro a la tabla Ingreso
+        /// </summary>
+        /// <returns></returns>
+        Task<RespuestaDB> AddIngreso(TransactionAggregate aggregate, string token);
+
+        /// <summary>
+        /// Realiza una transferencia ingresando datos a las tablas Gasto e Ingreso
+        /// </summary>
+        /// <returns></returns>
+        Task<RespuestaDB> AddTransferencia(TransferAggregate aggregate, string token);
+
+        /// <summary>
+        /// Obtiene todos los registros de las tablas Gasto e Ingreso de una billetera en especifico 
+        /// </summary>
+        /// <returns></returns>
+        Task<object> GetTransaccionesByIdWallet(IdWalletAggregate aggregate, string token);
+
+        /// <summary>
+        /// Obtiene todos los registros de las tablas Gasto e Ingreso de un usuario en especifico
+        /// </summary>
+        /// <returns></returns>
+        Task<object> GetTransaccionesByIdUsuario(string token);
+
+        
+    }
+}
--- LGG.LenayGestorGatos.Domain/Usings.cs
+++ LGG.LenayGestorGatos.Domain/Usings.cs
@@ -3,5 +3,6 @@ global using LGG.LenayGestorGatos.Domain.DTOs.Persona;
 global using LGG.LenayGestorGatos.Domain.Aggregates.Persona;
 global using LGG.LenayGestorGatos.Domain.DTOs;
 global using LGG.LenayGestorGatos.Domain.Aggregates.Usuario;
+global using LGG.LenayGestorGatos.Domain.Aggregates.Transactions;
 global using LGG.LenayGestorGatos.Domain.Aggregates.Wallet;
 global using LGG.LenayGestorGatos.Domain.DTOs.Wallet;
