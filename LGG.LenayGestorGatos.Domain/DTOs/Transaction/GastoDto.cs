dacbb1b2af815c2be3152120104b0289d8e17c0a
Israel Curiel <israelcuriel597@hotmail.com>
2024-11-03 01:49:41 -0600

    LGG: Modificacion de los campos(Adicion de nuevos campos)

--- LGG.LenayGestorGatos.Domain/DTOs/Transaction/GastoDto.cs
+++ LGG.LenayGestorGatos.Domain/DTOs/Transaction/GastoDto.cs
@@ -7,9 +7,12 @@ namespace LGG.LenayGestorGatos.Domain.DTOs.Transaction
         public int Id { get; set; }
         public string DescripcionDelGasto { get; set; }
         public DateTime Fecha { get; set; }
-        public string CategoriaIcono {  get; set; }
+        public string CategoriaIcono { get; set; }
         public string CategoriaColor { get; set; }
         public string CategoriaNombre { get; set; }
-        public decimal Dinero {  get; set; }
+        public decimal Dinero { get; set; }
+        public decimal BilleteraSaldo { get; set; }
+        public string BilleteraNombre { get; set; }
+        public string BilleteraColor { get; set; }
     }
 }
