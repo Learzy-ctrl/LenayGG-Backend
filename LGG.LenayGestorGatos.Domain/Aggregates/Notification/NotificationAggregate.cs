05c7ce4fdc8be0f527222843505fec3328bbf9fa
Israel Curiel <israelcuriel597@hotmail.com>
2024-11-28 04:14:15 -0600

    LGG: Aggregate notificacion

---
+++ LGG.LenayGestorGatos.Domain/Aggregates/Notification/NotificationAggregate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LGG.LenayGestorGatos.Domain.Aggregates.Notification
+{
+    public class NotificationAggregate
+    {
+        public string encabezado { get; set; }
+        public string cuerpo { get; set; }
+        public DateTime fecha { get; set; }
+        public string idUsuario { get; set; }
+    }
+}
