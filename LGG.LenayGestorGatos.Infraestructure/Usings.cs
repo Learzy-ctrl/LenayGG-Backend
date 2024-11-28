d48309a5992b5a92338f637f94ee05da7548da9d
Israel Curiel <israelcuriel597@hotmail.com>
2024-11-28 04:29:32 -0600

    LGG: Usings actualizados

--- LGG.LenayGestorGatos.Infraestructure/Usings.cs
+++ LGG.LenayGestorGatos.Infraestructure/Usings.cs
@@ -23,3 +23,5 @@ global using LGG.LenayGestorGatos.Domain.DTOs.Wallet;
 global using LGG.LenayGestorGatos.Domain.Aggregates.Reportes;
 global using LGG.LenayGestorGatos.Domain.Aggregates.Transactions;
 global using LGG.LenayGestorGatos.Domain.Aggregates.Wallet;
+global using LGG.LenayGestorGatos.Domain.DTOs.Notification;
+global using LGG.LenayGestorGatos.Domain.DTOs.Reportes;
